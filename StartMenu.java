48fcf5d1945cbae91b8505b2cdf67032f6eaf904
Pedro Rodrigues <pedroagrodrigues@gmail.com>
2018-04-06 19:36:02 +0100

    Add background
    Full background start menu

--- StartMenu.java
+++ StartMenu.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 public class StartMenu extends World
 {
     // Declaração de Variáveis
-    
+    GreenfootImage background;
     /**
      * Cria um mundo onde vamos ter as opçoes de jogo
      */
@@ -14,7 +14,10 @@ public class StartMenu extends World
        // Define o Tamanho do super de Acordo com a Resolução do Ecrã.
         super((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth()/3,
         (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()/1.5), 1);
-        
+        background = new GreenfootImage("MenuBackground.png");
+        background.scale(getWidth(), getHeight());
+        getBackground().drawImage(background, 0,0);
+                
         objectSpawn();
     }
     
@@ -23,8 +26,8 @@ public class StartMenu extends World
      */
     private void objectSpawn()
     {
-        addObject(new Button(0, getHeight()), (int)(getWidth()/3), (int)(getHeight()/2.5));
-        addObject(new Button(1, getHeight()), (int) (getWidth()*2/3), (int)(getHeight()/2.5));
-        addObject(new Button(2, getHeight()), (int) (getWidth()/2), (int)(getHeight()-getHeight()/3));
+        addObject(new Button(0, getHeight()), (int)(getWidth()/4), 100);
+        addObject(new Button(1, getHeight()), (int) ((getWidth()/2)+(getWidth()/3)), 100);
+        addObject(new Button(2, getHeight()), (int) (getWidth()/5), (int)(getHeight()-100));
     }
 }
